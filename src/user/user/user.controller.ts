8a3713b1a42f5af0288e3e217e3a3bf871e1072c
ariskaAdi <ariskaadiprasetyo@gmail.com>
2025-06-22 12:57:53 +0700

    async & response

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -3,13 +3,15 @@ import {
   Get,
   Header,
   HttpCode,
+  HttpRedirectResponse,
   Param,
   Query,
+  Redirect,
 } from '@nestjs/common';
 
 @Controller('api/users')
 export class UserController {
-  // http method return json and status cosde
+  // http method return json and status code responses
   @Get('/json')
   @Header('Content-Type', 'application/json')
   @HttpCode(200)
@@ -17,6 +19,25 @@ export class UserController {
     return {
       data: 'Hello World',
     };
+  }
+
+  //   redirect Responses
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/json',
+      statusCode: 301,
+    };
+  }
+
+  //   async responses
+  @Get('/hello')
+  async getHello(
+    @Query('firstName') firstName: string,
+    @Query('lastName') lastName: string,
+  ): Promise<string> {
+    return `Hello ${firstName} ${lastName} welcome back`;
   }
 
   // http method get
